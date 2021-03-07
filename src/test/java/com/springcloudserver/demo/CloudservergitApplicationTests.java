f30f1d20bb2314583c6399c680047280b684a33d
ahmed labib <ahmed.m.labeb@gmail.com>
2021-03-07 16:45:36 +0400

    Intial Commit

---
+++ src/test/java/com/springcloudserver/demo/CloudservergitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloudserver.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudservergitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
